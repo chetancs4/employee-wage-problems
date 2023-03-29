749ab46681d42420f2b2e6d9d5745ff88a11d136
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-03-29 13:07:39 +0530

    [Chetan] Add: The Script is the calculating Part time Empployee & Wage

--- emp.sh
+++ emp.sh
@@ -3,22 +3,30 @@
 echo "Welcome To Employee Wage Computation Program"
 
 # Variable for checking the attendence
-attendence_chk=$((RANDOM%2)) #0/1
+attendence_chk=$((RANDOM%3)) #0/1
 
 # constant variables
-is_present=1
+is_present_full_time=1
+is_present_part_time=2
 full_day_hour=8
+part_time_hour=4
 wage_per_hour=20
 
 # initializing the variables
 salary=0
 
 # To check the attendence of the employee
-if [ $attendence_chk -eq $is_present ]
+if [ $attendence_chk -eq $is_present_full_time ]
 then
     salary=$(($full_day_hour * $wage_per_hour))
     echo "The Employee is Present for Full day and the Salary is $salary"
+
+elif [ $attendence_chk -eq $is_present_part_time ]
+then
+     salary=$(($part_time_hour * $wage_per_hour))
+     echo "The Employee is Present for part time and the Salary is $salary"
+
 else
-    echo "Employee is Absent and the salary is $salary"
+     echo "Employee is Absent and the salary is $salary"
 fi
 
