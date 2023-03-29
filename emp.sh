a2e0f32226fdfb1ac5d4d77d8dcf23c8ae8b1ebf
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-03-29 12:15:31 +0530

    [Chetan] Add: The Script is the calculating the Wage of Daily employee

--- emp.sh
+++ emp.sh
@@ -1,17 +1,24 @@
-#!/bin/bash-x
+#!/bin/bash
 
 echo "Welcome To Employee Wage Computation Program"
 
-#variable
-emp_check=$((RANDOM%2)) # 0/1
+# Variable for checking the attendence
+attendence_chk=$((RANDOM%2)) #0/1
 
-#constant varible
+# constant variables
 is_present=1
+full_day_hour=8
+wage_per_hour=20
 
-#selection
-if [ $emp_check -eq $is_present ]
+# initializing the variables
+salary=0
+
+# To check the attendence of the employee
+if [ $attendence_chk -eq $is_present ]
 then
-echo "Employee is Present"
+    salary=$(($full_day_hour * $wage_per_hour))
+    echo "The Employee is Present for Full day and the Salary is $salary"
 else
-echo "Employee is Absent"
+    echo "Employee is Absent and the salary is $salary"
 fi
+
