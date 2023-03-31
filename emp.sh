3c6cca4c6fbc7a082a808438a2f96bcf6b923015
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-03-31 11:31:48 +0530

    [chetan] Add : This script is used to calculate the salry using switch case.

--- emp.sh
+++ emp.sh
@@ -3,7 +3,7 @@
 echo "Welcome To Employee Wage Computation Program"
 
 # Variable for checking the attendence
-attendence_chk=$((RANDOM%3)) #0/1
+attendence_chk=$((RANDOM%3)) #0/1/2
 
 # constant variables
 is_present_full_time=1
@@ -12,21 +12,22 @@ full_day_hour=8
 part_time_hour=4
 wage_per_hour=20
 
+
 # initializing the variables
 salary=0
+daily_hour=0
 
-# To check the attendence of the employee
-if [ $attendence_chk -eq $is_present_full_time ]
-then
-    salary=$(($full_day_hour * $wage_per_hour))
-    echo "The Employee is Present for Full day and the Salary is $salary"
+case $attendence_chk in
 
-elif [ $attendence_chk -eq $is_present_part_time ]
-then
-     salary=$(($part_time_hour * $wage_per_hour))
-     echo "The Employee is Present for part time and the Salary is $salary"
+$is_present_full_time) daily_hour=8
+                       echo "The Employee is Present for Full day"
+                       ;;
+$is_present_part_time) daily_hour=4
+                       echo "The Employee is Present for Half day"
+                       ;;
+0) echo "The Employee is Absent"
+;;
+esac
 
-else
-     echo "Employee is Absent and the salary is $salary"
-fi
-
+salary=$(($daily_hour * $wage_per_hour))
+echo "Salary of an Employee is $salary"
