990c9d3c363ce1ebfe7540cc165ae661950754a7
Chetan Sadvelkar <chetansadvelkar4@gmail.com>
2023-04-02 22:25:05 +0530

    [Chetan] Add : This script is used to Store the day and the Daily Wgae along with the Total Wage.

--- emp.sh
+++ emp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 get_work_hrs ()
+
 {   
     emp_attendence=$1
      case $emp_attendence in 
@@ -16,6 +18,7 @@ get_work_hrs ()
     echo "$no_of_hrs_per_day"
 }
 
+
 declare -A wage
 wage_per_hr=20
 max_hrs=100
@@ -23,16 +26,20 @@ max_days=20
 total_working_hrs=0
 total_working_days=0
 total_wage=0
-echo "daily wage total wage"
+echo "Day       Daily wage    Total wage"
+echo 
+
 
 while(( total_working_hrs < max_hrs && total_working_days < max_days ))
 do
     (( total_working_days++ ))
     emp_attendence=$(( RANDOM % 3 ))
     no_of_hrs_per_day=$( get_work_hrs $emp_attendence)
+    
     (( total_working_hrs += no_of_hrs_per_day ))
     daily_wage=$(( no_of_hrs_per_day * wage_per_hr ))
+    
     (( total_wage += daily_wage ))
     wage[$total_wage]=$daily_wage
-    echo "$daily_wage          $total_wage"
+    echo "Day${total_working_days}          $daily_wage           $total_wage"
 done
